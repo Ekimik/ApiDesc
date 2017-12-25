0b16f884512fbc135213f3c0175776f219a6cbf1
Jan Jíša <j.jisa@seznam.cz>
2017-12-25 16:55:23 +0100

    removing useless dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,5 @@
 <?php
 
-/**
- * Tests bootstrap file
- * @author Jan Jíša <j.jisa@seznam.cz>
- */
 require __DIR__ . '/../vendor/autoload.php';
 
-// app base directory
-define('TESTS_DIR', __DIR__);
-
-$loader = new Nette\Loaders\RobotLoader;
-$loader->addDirectory(__DIR__ . '/../src');
-$loader->setCacheStorage(new Nette\Caching\Storages\FileStorage(TESTS_DIR . '/tmp'));
-$loader->register();
 ?>
